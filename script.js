2d94b3a9aaaf9a8a016bb630734b76c8328494d2
Guanquan18 <s10257825@connect.np.edu.sg>
2025-02-04 03:29:49 +0800

    Added index.html for personal website

---
+++ script.js
@@ -0,0 +1,22 @@
+function topFunction() {
+    document.body.scrollTop = 300; // For Safari
+    document.documentElement.scrollTop = 300; // For Chrome, Firefox, IE and Opera
+}
+
+function clickLink(link) {
+    // Open a new tab with the link
+    window.open(link, '_blank');
+
+}
+
+function downloadResume() {
+    // Download the resume pdf from local folder
+    window.open(`./downloads/Chang Guan Quan's Tech Resume.pdf`, '_blank');
+}
+
+function scrollToSection(id) {
+    // Scroll to the element
+    const element = document.getElementById(id);
+    console.log(element);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
